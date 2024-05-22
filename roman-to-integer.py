d17c47ad35207108447e2d54ab4a294303e3dd6f
christopher-carlton <80734143+doublesync@users.noreply.github.com>
2024-05-22 12:54:33 -0400

    Update roman-to-integer.py

--- roman-to-integer.py
+++ roman-to-integer.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
         response = 0
         numerals = {
             "I": 1,
@@ -24,16 +20,11 @@ class Solution(object):
         }
         while s:
             char = s[0]
-            # first two characters of the string may have a 'case'
             potential_case = s[:2]
             if potential_case in cases:
-                # remove the characters
                 s = s.replace(potential_case, "", 1)
-                # add the value of the case to the response
                 response += cases[potential_case]
             elif char in numerals:
-                # remove the characters
                 s = s.replace(char, "", 1)
-                # add the value of the char to the response
                 response += numerals[char]
         return response
