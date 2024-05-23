6fed28c98a5829f4652f23e661c8007b8af2ac8b
christopher-carlton <80734143+doublesync@users.noreply.github.com>
2024-05-23 15:12:57 -0400

    Update remove-duplicates-from-sorted-array.py

--- remove-duplicates-from-sorted-array.py
+++ remove-duplicates-from-sorted-array.py
@@ -1,4 +1,5 @@
 class Solution(object):
     def removeDuplicates(self, nums):
+        # must sort 'in-place; hence 'nums[:]'
         nums[:] = sorted(set(nums)) # remove the duplicates; sort
         return len(nums) # return the length
