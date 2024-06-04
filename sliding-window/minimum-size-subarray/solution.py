c406293e063bc131778f885638d7decdd76151ff
christopher-carlton <80734143+doublesync@users.noreply.github.com>
2024-06-04 12:26:33 -0400

    Update solution.py

--- sliding-window/minimum-size-subarray/solution.py
+++ sliding-window/minimum-size-subarray/solution.py
@@ -1,20 +1,15 @@
 class Solution(object):
     def minSubArrayLen(self, target, nums):
-        """
-        :type target: int
-        :type nums: List[int]
-        :rtype: int
-        """
-        length = len(nums) 
-        shortest = float('inf')
-        left, total = 0, 0
+        length = len(nums) # the length of the nums array
+        shortest = float('inf') # initializing shortest to infinity (for easy 'min' use)
+        left, total = 0, 0 # initializing left pointer and total to zero
 
-        for right in range(length):
-            total += nums[right]
-            while total >= target:
-                w_length = (right - left) + 1
-                shortest = min(w_length, shortest)
-                total -= nums[left]
-                left += 1
+        for right in range(length): # iterate through each number in the array (number == right pointer)
+            total += nums[right] # increment total by number right pointer is at
+            while total >= target: # while total is greater/equal-to target value
+                w_length = (right - left) + 1 # the length of the window/subarray
+                shortest = min(w_length, shortest) # set shortest to shortest length
+                total -= nums[left] # decrement total
+                left += 1 # increment left pointer
 
-        return 0 if shortest == float('inf') else shortest
+        return 0 if shortest == float('inf') else shortest # return shortest based on value
